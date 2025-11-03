b9f9e981cdf61d7c3214bde6252491368eeb2833
san04git <bsanchita24@tbc.edu.np>
2025-11-03 14:27:18 +0545

    Create Task4sweet.py

---
+++ Task4sweet.py
@@ -0,0 +1,5 @@
+sweet = int(input("How many sweets are there in total? "))
+pupils = int(input("How many pupils are there today? "))
+per_puplis = sweets//puplis
+left_over = sweet % puplis
+print(f"Each pupli will get"
